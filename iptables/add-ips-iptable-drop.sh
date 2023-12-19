3ddf86d03a9a796198cd069af3a49e3599f7d417
tidycozydev <tidycozydev@proton.me>
2023-12-19 16:40:09 -0500

    add check for rule already existing

--- iptables/add-ips-iptable-drop.sh
+++ iptables/add-ips-iptable-drop.sh
@@ -6,10 +6,14 @@ if test $(cut -d '.' -f1 /proc/uptime) -lt 86400; then
     /sbin/iptables-restore < /bash-scripts/iptables.backup
 fi
 
-# we add a conf in iptables for every IP in our file
+# we add an iptable rule for every IP in our file
+# we check if the rule does not exist already in case the server restarted and we got annoyed again by
+# the same guy meanwhile before restoring the backup
 while read -r line
 do
-    /sbin/iptables -A INPUT -s "$line" -j DROP
+    if $(/sbin/iptables -C INPUT -s $line -j DROP); then
+        /sbin/iptables -A INPUT -s "$line" -j DROP
+    fi
 done < "$csv_file"
 
 # finally we update the iptables backup
