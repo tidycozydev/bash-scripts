2cc8cb2fd3dd8ad3cb99f97ff2be62b4ad11aa2e
tidycozydev <tidycozydev@proton.me>
2023-12-20 08:33:10 -0500

    rename script

--- iptables/add-ips-iptables-drop.sh
+++ iptables/add-ips-iptables-drop.sh
@@ -7,8 +7,8 @@ if test $(cut -d '.' -f1 /proc/uptime) -lt 86400; then
 fi
 
 # we add an iptable rule for every IP in our file
-# we check if the rule does not exist already in case the server restarted and we got annoyed again by
-# the same guy meanwhile before restoring the backup
+# we check if the rule does not exist already in case the server restarted and 
+# we got annoyed again by the same guy meanwhile, before restoring the backup
 while read -r line
 do
     if ! $(/sbin/iptables -C INPUT -s $line -j DROP); then
